c98ebe255725f607919ac4ee1b3dab46ffdb2315
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-10-21 15:19:25 -0400

    Scaffold items

---
+++ db/migrate/20221021191814_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :type
+      t.integer :status
+      t.string :clay_body
+      t.string :glazes
+      t.float :height
+      t.float :width
+      t.string :memo
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
