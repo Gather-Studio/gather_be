11b1f7781b4a610556c38bacebb8a646192fc012
Jennifer Halloran <jenniferlhalloran@gmail.com>
2023-01-04 10:02:43 -0500

    Create sessions route and controller action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::SessionsController < ApplicationController 
+  
+  # POST /api/v1/sessions
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :users do 
         resources :items, controller: :user_items
       end
+      resources :sessions, only: [:create]
     end
   end
 end
