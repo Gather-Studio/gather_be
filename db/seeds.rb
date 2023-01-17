62df444813d055f3fd3d6a6e695866ff58d6ba80
Jennifer Halloran <jenniferlhalloran@gmail.com>
2023-01-17 16:55:29 -0500

    Add admin role to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,13 @@ user3 = User.create!(email: "nick@newgirl.com",
                     password_confirmation: "clydesbar",
                     first_name: "Nick",
                     last_name: "Miller")
+
+admin = User.create!(email: "admin@gmail.com",
+                     password: "rubyisfun",
+                     password_confirmation: "rubyisfun",
+                     first_name: "Example",
+                     last_name: "Admin",
+                     role: 1)
  
 ##### ITEMS
 
@@ -38,6 +45,42 @@ user1.items.create!(name: "Spoon",
                     width: 4.5, 
                     memo: "Gift for Nan", 
                     style: "Hand Built")
+
+user1.items.create!(name: "Fork", 
+                    status: 1, 
+                    clay_body: "Red Clay", 
+                    glazes: "Blue Rutile x2", 
+                    height: 10.0, 
+                    width: 4.5, 
+                    memo: "To go with the spoon", 
+                    style: "Hand Built")
+
+user1.items.create!(name: "Knife", 
+                    status: 2, 
+                    clay_body: "Red Clay", 
+                    glazes: "Blue Rutile x2", 
+                    height: 10.0, 
+                    width: 4.5, 
+                    memo: "To go with the Fork", 
+                    style: "Hand Built")
+
+user1.items.create!(name: "Soup Bowl", 
+                    status: 3, 
+                    clay_body: "Red Clay", 
+                    glazes: "Blue Rutile x2", 
+                    height: 7.0, 
+                    width: 5.0, 
+                    memo: "Almost a complete set", 
+                    style: "Wheel-Thrown")
+
+user1.items.create!(name: "Plate", 
+                    status: 4, 
+                    clay_body: "Red Clay", 
+                    glazes: "Blue Rutile x2", 
+                    height: 1.0, 
+                    width: 10.0, 
+                    memo: "Final Piece!", 
+                    style: "Wheel-Thrown")
 
 user2.items.create!(name: "Cat Bowl", 
                     status: 3, 
